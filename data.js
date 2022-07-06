a5cbe69b85437c30778de4872281a031c75542be
Anderson Coral <anderson_coral@outlook.com>
2022-07-05 21:11:56 -0500

    actualizando archivo data.js

--- data.js
+++ data.js
@@ -2,25 +2,23 @@ const listUser = [
     {
         name: 'Asencio Tica, Martin',
         image: 'Link'
-    },
-    {
+    },{
         name: 'Mundaca Siesquen, Luiggi',
         image: 'Link'
-    },
-    {
+    },{
         name: 'Rueda Amaya, Joseph',
         image: 'Link'
-    },
-    {
+    },{
         name: 'Namuche Zapata, Andersson',
         image: 'Link'
-    },
-    {
+    },{
         name: 'BILL ANDERSSON NAMUCHE ZAPATA',
         image: 'Link'
-    },
-    {
+    },{
         name: 'MIJAEL RODRIGUEZ ALATA',
-        Image: 'Link'
+        image: 'Link'
+    },{
+        name: 'Anderson Coral Zamora',
+        image: 'Link'
     }
 ];
