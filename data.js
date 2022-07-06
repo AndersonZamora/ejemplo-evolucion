378a5c929024d13009da46fb604575da750975a6
Anderson Coral <anderson_coral@outlook.com>
2022-07-05 20:53:05 -0500

    commit inicial

---
+++ data.js
@@ -0,0 +1,26 @@
+const listUser = [
+    {
+        name: 'Asencio Tica, Martin',
+        image: 'Link'
+    },
+    {
+        name: 'Mundaca Siesquen, Luiggi',
+        image: 'Link'
+    },
+    {
+        name: 'Rueda Amaya, Joseph',
+        image: 'Link'
+    },
+    {
+        name: 'Namuche Zapata, Andersson',
+        image: 'Link'
+    },
+    {
+        name: 'BILL ANDERSSON NAMUCHE ZAPATA',
+        image: 'Link'
+    },
+    {
+        name: 'MIJAEL RODRIGUEZ ALATA',
+        Image: 'Link'
+    }
+];
---
+++ modelUser.js
@@ -0,0 +1,5 @@
+
+const modelUser = {
+    name: 'Anderson',
+    image: 'Link'
+}
